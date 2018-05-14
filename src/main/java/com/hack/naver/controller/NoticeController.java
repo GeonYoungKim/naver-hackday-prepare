63833a7f60e13d69f4d51bcf7983ecc4cb9210ab
geonyoungkim <c2619zz@naver.com>
2018-05-14 18:16:48 +0900

    다중 파일 업로드 1.  요구사항에 맞도록  png, xslx, xls, txt, csv, tsv의 파일들만 업로드. 2. 크기는 5MB이하로 개발 3. 복수개 업로드 가능

--- src/main/java/com/hack/naver/controller/NoticeController.java
+++ src/main/java/com/hack/naver/controller/NoticeController.java
@@ -11,12 +11,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.google.gson.JsonObject;
 import com.hack.naver.service.LoginService;
 import com.hack.naver.service.NoticeService;
 
